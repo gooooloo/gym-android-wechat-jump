e1b6000b1f1ba353341030cc07fd037c22697257
gooooloo <qidu.lin@gmail.com>
2019-02-13 00:37:18 +0800

    use config in ocr.py as well

--- ocr.py
+++ ocr.py
@@ -1,39 +1,41 @@
 import cv2
 import os
+from config import cfg as cfg
 
 
 def ocr(img):
-    assert img.shape == (85,69)
+    assert img.shape == (cfg.SCORE_DIGIT_HEIGHT, cfg.SCORE_DIGIT_WIDTH)
 
-    COLOR_TARGET = 72
-    myint = None
+    color_target = cfg.SCORE_COLOR_TARGET
+    my_int = None
 
-    if img[1][1] != COLOR_TARGET:
-        myint = None
-    elif img[25][25] == COLOR_TARGET:
-        myint = 1
-    elif img[60][25] == COLOR_TARGET:
-        if img[40][40] == COLOR_TARGET:
-            myint = 7
+    # TODO: the magic numbers ( like 25, 60, etc) belongs to cfg as well...
+    if img[1][1] != color_target:
+        my_int = None
+    elif img[25][25] == color_target:
+        my_int = 1
+    elif img[60][25] == color_target:
+        if img[40][40] == color_target:
+            my_int = 7
         else:
-            myint = 4
-    elif img[40][40] != COLOR_TARGET:
-        myint = 0
-    elif img[60][60] != COLOR_TARGET:
-        myint = 2
-    elif img[25][5] != COLOR_TARGET:
-        myint = 3
-    elif img[60][5] != COLOR_TARGET:
-        if img[25][60] != COLOR_TARGET:
-            myint = 5
+            my_int = 4
+    elif img[40][40] != color_target:
+        my_int = 0
+    elif img[60][60] != color_target:
+        my_int = 2
+    elif img[25][5] != color_target:
+        my_int = 3
+    elif img[60][5] != color_target:
+        if img[25][60] != color_target:
+            my_int = 5
         else:
-            myint = 9
-    elif img[25][60] != COLOR_TARGET:
-            myint = 6
+            my_int = 9
+    elif img[25][60] != color_target:
+            my_int = 6
     else:
-        myint = 8
+        my_int = 8
 
-    return int(myint) if myint is not None else None
+    return int(my_int) if my_int is not None else None
 
 
 def main():
@@ -41,12 +43,12 @@ def main():
 
     for i in range(10):
         img = cv2.imread(f'{d}/score_png_as_ref/{i}.png', cv2.IMREAD_GRAYSCALE)
-        myint = ocr(img)
-        assert myint == i, f'expect {i}, actual {myint}'
+        my_int = ocr(img)
+        assert my_int == i, f'expect {i}, actual {my_int}'
 
     img = cv2.imread(f'{d}/score_png_as_ref/none.png', cv2.IMREAD_GRAYSCALE)
-    myint = ocr(img)
-    assert myint is None, f'expect {None}, actual {myint}'
+    my_int = ocr(img)
+    assert my_int is None, f'expect {None}, actual {my_int}'
 
 
 if __name__ == '__main__':
